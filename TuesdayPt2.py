12731c4bc5922534fa60c27add7a8340f30936f2
mhersom <madison.hersom@gmail.com>
2023-12-07 17:32:09 +0000

    TuesdayWork

---
+++ TuesdayPt2.py
@@ -0,0 +1,30 @@
+"""
+Tuesday - 2nd coding exercise
+"""
+
+#Reassigning
+#age = 28
+#print (age)
+#new_age = age+5
+#print(new_age)
+
+#age += 7
+#age *= 2
+#age -5 = 5
+
+text = 'hokus' + 'pokus'
+print(text)
+print('hokus' * 5)
+print (23+5)
+print ('23'+'5')
+
+#input
+#print ('What is your name?')
+#user_name = input ()
+#print ('Hello', user_name)
+
+#Coding Exercise
+login = input('Enter your login: ')
+language = input ('Enter your native language: ')
+print ('Your login is', login, 'and you speak', language)
+

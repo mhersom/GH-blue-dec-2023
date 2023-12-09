8444210c5d3ab3f0d2b275bc426cd912e47095bd
mhersom <madison.hersom@gmail.com>
2023-12-09 17:16:39 +0000

    Friday_1208

---
+++ Friday_1208.py
@@ -0,0 +1,14 @@
+"""
+Friday 12/08
+"""
+
+#Coding exercise
+while True:
+    answer = int(input('When was Python 1.0 released? '))
+    if answer > 1994:
+        print('It was earlier than that!')
+    elif answer < 1994:
+        print('It was later than that!')
+    else:
+        print('Correct!')
+        break
